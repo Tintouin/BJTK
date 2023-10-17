705b4030b251160e2dc501a6e41715df4cc60388
Lina <110615697+Linliiiin@users.noreply.github.com>
2023-10-17 09:39:25 +0200

    Update cartetest.py

--- cartetest.py
+++ cartetest.py
@@ -11,6 +11,7 @@ class Carte():
         self.hauteur = hauteur
         self.couleur = couleur
         self.imageCarte = None  # Variable pour stocker l'image
+        self.pos=[0,0]
 
     def __repr__(self):
         dicolor = {"P": " de Pique", "C": " de Coeur", "T": " de Trèfle", "K": " de Carreau"}
@@ -27,34 +28,15 @@ class Carte():
         self.imageCarte = ImageTk.PhotoImage(image_resized)
         label = tk.Label(parent, image=self.imageCarte)
         label.image = self.imageCarte  # Garder une référence à l'image
-        label.pack()
+        label.grid(row=self.pos[0]+1, column=self.pos[1]+1, padx=10 )  # Utilisation de grid pour l'affichage horizontal
 
-class PaquetDeCarte():
-    def __init__(self, paquets):
-        self.paquets = paquets
+pioche=[]
+croupier=[]
+joueur=[]
 
-    def est_vide(self):
-        return len(self.paquets) == 0
-
-    def taille(self):
-        return len(self.paquets)
-
-    def battre(self):
-        random.shuffle(self.paquets)
-
-def piocher_carte():
-    if paquet.paquets:
-        carte_au_hasard = random.choice(paquet.paquets)
-        carte_au_hasard.image(fenetre, resize_factor=0.3)
-        paquet.paquets.remove(carte_au_hasard)
 
 # Créer un bouton pour piocher une carte
-bouton_piocher = tk.Button(fenetre, text="Piocher une carte", command=piocher_carte)
-bouton_piocher.pack()
-
-paquet = PaquetDeCarte([Carte(1, "P"), Carte(2, "T"), Carte(3, "C")])
-
-fenetre.mainloop()
-
+bouton_piocher = tk.Button(fenetre, text="Piocher une carte", command=1+1)
+bouton_piocher.grid(row=5)
 
 fenetre.mainloop()
